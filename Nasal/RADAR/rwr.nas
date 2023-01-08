swampmo/Jaguar-GR1

commit 773f3dc00e727bc2da13615726d524f9eb421f85

RWR Update (#67)
* RWR: Make it depend on elec
RWR: Update underlying radar code.
Arms: Update missile-code and vector.nas
Stores: Update store code.
EMU: Comment a battery line that used the wrong property name. Its set in engine start anyway.

* RWR: Depend on better power source

Nasal/RADAR/rwr.nas
@@ -1,6 +1,6 @@
 print("*** LOADING rwr.nas ... ***");
 
-var lineWidth = 3;
+var lineWidth = 2;
 
 RWRCanvas = {
     new: func (_ident, root, center, diameter) {
@@ -179,6 +179,8 @@ RWRCanvas = {
 #        }
 # Threat list ID:
 
+        #REVISION: 2022/01/16
+        #OPRF Fleet
         rwr.AIRCRAFT_WARTHOG  = "10";
         rwr.AIRCRAFT_TOMCAT   = "14";
         rwr.AIRCRAFT_EAGLE    = "15";
@@ -201,6 +203,7 @@ RWRCanvas = {
         #MISC
         rwr.AIRCRAFT_FAGOT    = "MG";
         rwr.AIRCRAFT_FOXBAT   = "FB";
+        rwr.AIRCRAFT_FLOGGER  = "23";
         rwr.AIRCRAFT_FULLBACK = "34";
         rwr.AIRCRAFT_PAKFA    = "57";
         rwr.AIRCRAFT_TYPHOON  = "EF";
@@ -300,6 +303,8 @@ RWRCanvas = {
                 "F-4EJ_ADTW":               rwr.AIRCRAFT_PHANTOM,
                 "FGR2-Phantom":             rwr.AIRCRAFT_PHANTOM,
                 "F4J":                      rwr.AIRCRAFT_PHANTOM,
+                "F-4D":                     rwr.AIRCRAFT_PHANTOM,
+                "F-4E":                     rwr.AIRCRAFT_PHANTOM,
                 "F-4N":                     rwr.AIRCRAFT_PHANTOM,
                 "a4f":                      rwr.AIRCRAFT_SKYHAWK,
                 "A-4K":                     rwr.AIRCRAFT_SKYHAWK,
@@ -327,9 +332,11 @@ RWRCanvas = {
                 "mb339pan":                 rwr.AIRCRAFT_MB339,
                 "alphajet":                 rwr.AIRCRAFT_ALPHAJET,
                 "MiG-15bis":                rwr.AIRCRAFT_FAGOT,
+                "MiG-23MLD":                rwr.AIRCRAFT_FLOGGER,
                 "Su-25":                    rwr.AIRCRAFT_FROGFOOT,
                 "MiG-25":                   rwr.AIRCRAFT_FOXBAT,
                 "A-6E-model":               rwr.AIRCRAFT_INTRUDER,
+                "A-6E":                     rwr.AIRCRAFT_INTRUDER,
                 "F-117":                    rwr.AIRCRAFT_NIGHTHAWK,
                 "F-22-Raptor":              rwr.AIRCRAFT_RAPTOR,
                 "F-35A":                    rwr.AIRCRAFT_JSF,
@@ -357,34 +364,33 @@ RWRCanvas = {
                 "V-1":                      rwr.AIRCRAFT_UFO,
                 "SpaceShuttle":             rwr.AIRCRAFT_UFO,
                 "F-23C_BlackWidow-II":      rwr.AIRCRAFT_UFO,
-        };        rwr.shownList = [];
+        };
+        rwr.shownList = [];
         #
         # recipient that will be registered on the global transmitter and connect this
         # subsystem to allow subsystem notifications to be received
         rwr.recipient = emesary.Recipient.new(_ident);
         rwr.recipient.parent_obj = rwr;
+        rwr.vector_aicontacts = [];
 
         rwr.recipient.Receive = func(notification)
         {
             if (notification.NotificationType == "FrameNotification")
             {
-                #
-                # Link16 wingmen only visible when no other threats. So check the size of this list
-                # first and if populated use it.
-	        if (notification["rwrList"] != nil and size(notification.rwrList)>0)
-                  me.parent_obj.update(notification.rwrList, "normal");
-                else if (notification["rwrList16"] != nil)
-                  me.parent_obj.update(notification.rwrList16, "link16");
+                #printf("RWR-canvas recv: %s", notification.NotificationType);
+                me.parent_obj.update();
                 return emesary.Transmitter.ReceiptStatus_OK;
             }
+            
             return emesary.Transmitter.ReceiptStatus_NotProcessed;
         };
         emesary.GlobalTransmitter.Register(rwr.recipient);
 
         return rwr;
     },
-    update: func (list, type) {
-	var s = size(list);
+    update: func {
+        var list = radar_system.getRWRList();
+	    var s = size(list);
         me.elapsed = getprop("sim/time/elapsed-sec");
         var sorter = func(a, b) {
             if(a[1] > b[1]){
@@ -395,15 +401,15 @@ RWRCanvas = {
                 return 1; # A should after b in the returned vector
             }
         }
-        me.sortedlist = sort(list, sorter);
+        me.sortedlist = sort(list, sorter);#sort threat
         me.newList = [];
         me.i = 0;
         me.prio = 0;
         me.newsound = 0;
         me.unk = 0;
         foreach(me.contact; me.sortedlist) {
-	    # print("rwr: " ~ me.contact[0].get_Callsign() ~ " " ~ me.contact[0].get_model() ~ " " ~ me.contact[1]);
-            me.typ=me.lookupType[me.contact[0].get_model()];
+	       #print("rwr: " ~ me.contact[0].get_Callsign() ~ " " ~ me.contact[0].getModel() ~ " " ~ me.contact[1]);
+            me.typ=me.lookupType[me.contact[0].getModel()];
 	    # printf("type: %d, threat: %d", me.typ, me.contact[1]);
             if (me.i > me.max_icons-1) {
                 break;
@@ -426,10 +432,10 @@ RWRCanvas = {
             } else {
                 continue;
             }
-            if (me.contact[0].get_range() > 110) {
+            if (me.contact[0].get_range() > 130) {
                 continue;
             }
-            me.dev = -geo.normdeg180(me.contact[0].get_bearing()-getprop("orientation/heading-deg"))+90;
+            me.dev = -me.contact[2]+90;
             me.x = math.cos(me.dev*D2R)*me.threat;
             me.y = -math.sin(me.dev*D2R)*me.threat;
             me.texts[me.i].setTranslation(me.x,me.y);
@@ -440,7 +446,7 @@ RWRCanvas = {
                 me.symbol_priority.show();
                 me.prio = 1;
             }
-            if (!(me.typ == me.ASSET_GARGOYLE or me.typ == me.ASSET_BUK or me.typ == me.ASSET_FRIGATE) and me.contact[0].get_Speed()>60) {
+            if (!(me.typ == me.ASSET_GARGOYLE or me.typ == me.ASSET_AAA or me.typ == me.ASSET_VOLGA or me.typ == me.ASSET_BUK or me.typ == me.ASSET_PAC2 or me.typ == me.ASSET_FRIGATE) and me.contact[0].get_Speed()>60) {
                 #air-borne
                 me.symbol_hat[me.i].setTranslation(me.x,me.y);
                 me.symbol_hat[me.i].show();
@@ -455,7 +461,7 @@ RWRCanvas = {
             }
             me.popupNew = me.elapsed;
             foreach(me.old; me.shownList) {
-                if(me.old[0].getUnique()==me.contact[0].getUnique()) {
+                if(me.old[0].equals(me.contact[0])) {
                     me.popupNew = me.old[1];
                     break;
                 }
@@ -503,6 +509,7 @@ RWRCanvas = {
 };
 var rwr = nil;
 var cv = nil;
+var timer = nil;
 
 var main_init_listener = setlistener("sim/signals/fdm-initialized", func {
    if (getprop("sim/signals/fdm-initialized") == 1) {
@@ -519,6 +526,8 @@ var main_init_listener = setlistener("sim/signals/fdm-initialized", func {
      var root = cv.createGroup();
      rwr = RWRCanvas.new("RWRCanvas", root, [diam/2,diam/2],diam*0.9);
      removelistener(main_init_listener);
+     timer = maketimer(0.5, func rwr.update());
+     timer.start();
    }
 }, 0, 0);
 
@@ -546,3 +555,4 @@ var setGroup = func (root) {
     root.createChild("path").horiz(768).vert(576).horiz(-768).vert(-576).setColorFill(0,0,0).setColor(0,0,0);
     rwr = RWRCanvas.new("RWRCanvas",root, [768/2,576/2],576);
 };
+

Jaguar-GR1-set.xml
@@ -280,6 +280,7 @@
         <int n="1" alias="/controls/switches/anti-collision-lights" />
 
         <!-- int[2] reserved for radar -->
+        <int type="int" n="2">  1 </int>
 
         <!-- Landing lights on -->
         <int n="3" alias="/controls/switches/landing-light" />
@@ -973,7 +974,12 @@
     <armament>
       <enable-craters type="bool">true</enable-craters>
       <models type="string">Aircraft/Jaguar-GR1/Models/Weapons/</models>
+      <MLW-bearing type="double">0</MLW-bearing>
       <MLW-count type="int">0</MLW-count>
+      <MLW-launcher type="string"></MLW-launcher>
+      <MAW-bearing type="double">0</MAW-bearing>
+      <MAW-active type="bool">false</MAW-active>
+      <spike type="bool">false</spike>
       <modelsUseCase type="bool">true</modelsUseCase>
       <modelsUpperCase type="bool">true</modelsUpperCase>
       <msg type="bool">false</msg>
@@ -1376,23 +1382,14 @@
       <file>Aircraft/Instruments-3d/radardist/radardist.nas</file>
     </radardist>
 
-    <radar>
-      <file>Aircraft/Jaguar-GR1/Nasal/RADAR/radar2.nas</file>
-      <file>Aircraft/Jaguar-GR1/Nasal/RADAR/target_class.nas</file>
-      <file>Aircraft/Jaguar-GR1/Nasal/RADAR/radarTool.nas</file>
-    </radar>
-
-    <rcs>
-      <file>Aircraft/Jaguar-GR1/Nasal/rcs.nas</file>
-    </rcs>
+    <radar_system>
+      <file>Aircraft/Jaguar-GR1/Nasal/RADAR/radar-system.nas</file>
+      <file>Aircraft/Jaguar-GR1/Nasal/RADAR/rwr-only.nas</file>
+    </radar_system>
 
     <rwr>
       <file>Aircraft/Jaguar-GR1/Nasal/RADAR/rwr.nas</file>
     </rwr>
-
-    <myradar>
-      <file>Aircraft/Jaguar-GR1/Nasal/RADAR/main.nas</file>
-    </myradar>
 
     <hud>
       <file>Aircraft/Jaguar-GR1/Nasal/HUD/hud_math.nas</file>

Models/Interior/Panel/3D-Models/Pylons.xml
@@ -47,7 +47,7 @@
   <animation>
     <type>rotate</type>
     <object-name>P0-arm</object-name>
-        <property>controls/armament/master-arm</property>
+        <property>controls/armament/master-arm-switch</property>
     <factor> 60 </factor>
     <axis>
       <x> 0 </x>
@@ -69,7 +69,7 @@
       <button>0</button>
       <binding>
         <command>property-toggle</command>
-        <property>controls/armament/master-arm</property>
+        <property>controls/armament/master-arm-switch</property>
       </binding>
     </action>
   </animation>
@@ -110,7 +110,7 @@
   <animation>
     <type>rotate</type>
     <object-name>P1-arm</object-name>
-        <property>controls/armament/master-arm</property>
+        <property>controls/armament/master-arm-switch</property>
     <factor> 60 </factor>
     <axis>
       <x> 0 </x>
@@ -132,7 +132,7 @@
       <button>0</button>
       <binding>
         <command>property-toggle</command>
-        <property>controls/armament/master-arm</property>
+        <property>controls/armament/master-arm-switch</property>
       </binding>
     </action>
   </animation>
@@ -173,7 +173,7 @@
   <animation>
     <type>rotate</type>
     <object-name>centre-arm</object-name>
-        <property>controls/armament/master-arm</property>
+        <property>controls/armament/master-arm-switch</property>
     <factor> 60 </factor>
     <axis>
       <x> 0 </x>
@@ -195,7 +195,7 @@
       <button>0</button>
       <binding>
         <command>property-toggle</command>
-        <property>controls/armament/master-arm</property>
+        <property>controls/armament/master-arm-switch</property>
       </binding>
     </action>
   </animation>
@@ -236,7 +236,7 @@
   <animation>
     <type>rotate</type>
     <object-name>S0-arm</object-name>
-        <property>controls/armament/master-arm</property>
+        <property>controls/armament/master-arm-switch</property>
     <factor> 60 </factor>
     <axis>
       <x> 0 </x>
@@ -258,7 +258,7 @@
       <button>0</button>
       <binding>
         <command>property-toggle</command>
-        <property>controls/armament/master-arm</property>
+        <property>controls/armament/master-arm-switch</property>
       </binding>
     </action>
   </animation>
@@ -299,7 +299,7 @@
   <animation>
     <type>rotate</type>
     <object-name>S1-arm</object-name>
-        <property>controls/armament/master-arm</property>
+        <property>controls/armament/master-arm-switch</property>
     <factor> 60 </factor>
     <axis>
       <x> 0 </x>
@@ -321,7 +321,7 @@
       <button>0</button>
       <binding>
         <command>property-toggle</command>
-        <property>controls/armament/master-arm</property>
+        <property>controls/armament/master-arm-switch</property>
       </binding>
     </action>
   </animation>

Models/Interior/Panel/Instruments/Radar/Radar/A-10-rwr.ac
@@ -1,6 +1,7 @@
 AC3Db
 MATERIAL "ac3dmat1" rgb 1 1 1  amb 0.2 0.2 0.2  emis 0 0 0  spec 0.2 0.2 0.2  shi 128  trans 0
 MATERIAL "ac3dmat1.007" rgb 1 1 1  amb 0.2 0.2 0.2  emis 0 0 0  spec 0 0 0  shi 62.6526  trans 0
+MATERIAL "Canvas" rgb 0 0 0  amb 0.0 0.0 0.0  emis 0 0 0  spec 0 0 0  shi 62.6526  trans 0
 OBJECT world
 kids 6
 OBJECT poly
@@ -5228,7 +5229,7 @@ numvert 4
 -3.32322 0.1774 -0.270974
 numsurf 1
 SURF 0x0
-mat 1
+mat 2
 refs 4
 2 0 0
 3 1 0

Models/Interior/Panel/Instruments/Radar/Radar/A-10-rwr.xml
@@ -6,8 +6,9 @@
 
   <animation> 
    <type>material</type>
+   <object-name>RWRScreen</object-name>
     <emission>
-      <factor-prop>controls/lighting/instruments-norm</factor-prop>
+      <factor-prop>fdm/jsbsim/systems/electrical/generic-pwr</factor-prop>
       <red>   0.9 </red>
       <green> 0.9 </green>
       <blue>  0.9 </blue>

Models/Interior/Panel/Instruments/mfd/round-mfd1.xml
@@ -26,7 +26,7 @@
     <object-name>Mode</object-name>
     <object-name>Scale</object-name>
     <condition>
-      <property>controls/electrical/battery-switch</property>
+      <property>fdm/jsbsim/systems/electrical/generic-pwr</property>
     </condition>
     <emission>
       <factor-prop>controls/lighting/instruments-norm</factor-prop>
@@ -44,7 +44,7 @@
         <property>sim/rendering/rembrandt/enabled</property>
         <value type="bool">true</value>
       </equals>
-      <property>controls/electrical/battery-switch</property>
+      <property>fdm/jsbsim/systems/electrical/generic-pwr</property>
     </condition>
     <diffuse>
       <factor-prop>controls/lighting/instruments-norm</factor-prop>

Nasal/HUD/HUD.nas
@@ -1273,7 +1273,7 @@ var HUD = {
       ILS_gs_deg:  "/instrumentation/nav/gs-direct-deg",
       NavHeadingNeedleDeflectionILS:"/instrumentation/nav/heading-needle-deflection-norm",
       
-      MasterArm      :"/controls/armament/master-arm",
+      MasterArm      :"/controls/armament/master-arm-switch",
       TimeToTarget   :"/sim/dialog/groundtTargeting/time-to-target",
     };
     

Nasal/HUD/hud_math.nas
@@ -2,7 +2,7 @@
 #
 # Author: Nikolai V. Chr. (FPI location code adapted from Buccaneer aircraft)
 #
-# Version 1.07
+# Version 1.09
 #
 # License: GPL 2.0
 	
@@ -57,13 +57,13 @@ var HudMath = {
 			me.centerOffsetSlantedMeter = -1*(me.length*0.5-me.boreSlantedDownFromTopMeter);#used (distance from center origin up to bore [negative number])
 			#printf("len=%.3fm angle=%.1fdeg angle2=%.1fdeg boredist=%.3fm borefromtop=%.3fm offset=%.3fm",me.length,me.slantAngle*R2D,me.slantAngleOther*R2D,me.distanceToBore,me.boreSlantedDownFromTopMeter,me.centerOffsetSlantedMeter);
 		}
-			if (initialization) {
-				# calc Y offset from HUD canvas center origin.
-				me.centerOffset = -1 * (me.canvasHeight/2 - ((me.hud3dTop - me.input.view0Z.getValue())*me.pixelPerMeterY));#TODO: use originCanvas?
-			} elsif (!me.parallax) {
-				# calc Y offset from HUD canvas center origin.
-				me.centerOffset = -1 * (me.canvasHeight/2 - ((me.hud3dTop - me.input.viewZ.getValue())*me.pixelPerMeterY));
-			}
+		if (initialization) {
+			# calc Y offset from HUD canvas center origin.
+			me.centerOffset = -1 * (me.canvasHeight/2 - ((me.hud3dTop - me.input.view0Z.getValue())*me.pixelPerMeterY));#TODO: use originCanvas?
+		} elsif (!me.parallax) {
+			# calc Y offset from HUD canvas center origin.
+			me.centerOffset = -1 * (me.canvasHeight/2 - ((me.hud3dTop - me.input.viewZ.getValue())*me.pixelPerMeterY));
+		}
 		
 	},
 	
@@ -95,14 +95,12 @@ var HudMath = {
 	
 	getBorePos: func {
 		# returns bore pos in canvas from center origin
+		if (me.slanted) {
+			return [0,me.centerOffsetSlantedMeter*me.pixelPerMeterYSlant];
+		}
 		return [0,me.centerOffset];
 	},
-	
-	getBorePosSlanted: func {
-		# returns bore pos in canvas from center origin
-		return [0,me.centerOffsetSlantedMeter*me.pixelPerMeterYSlant];
-	},
-	
+		
 	getPosFromCoord: func (gpsCoord, aircraft = nil) {
 		# return pos in canvas from center origin
 		if (aircraft== nil) {
@@ -143,8 +141,57 @@ var HudMath = {
 
 	    me.pos = me.getCenterPosFromDegs(me.dir_x,-me.dir_y);
 	    
-	    return [me.pos[0], me.pos[1]];
+	    return [me.pos[0], me.pos[1], me.dir_x,-me.dir_y];
 	},
+
+	getDevFromCoord: func (gpsCoord, viewh, viewp, hdp, aircraft=nil) {
+        # hdp is a hash containing pitch, roll, and true-heading of aircraft
+        # return pos in canvas from center origin
+        if (aircraft== nil) {
+            me.crft = geo.viewer_position();
+        } else {
+            me.crft = aircraft;
+        }
+        me.ptch = vector.Math.getPitch(me.crft, gpsCoord);
+        me.brng = me.crft.course_to(gpsCoord);
+
+        var ym = vector.Math.yawMatrix(-viewh);
+        var pm = vector.Math.pitchMatrix(-viewp);
+        var vm = vector.Math.multiplyMatrices(pm, ym);
+        me.rollM  = vector.Math.rollMatrix(-hdp.roll);
+        me.pitchM = vector.Math.pitchMatrix(-hdp.pitch);
+        me.yawM   = vector.Math.yawMatrix(hdp.heading);
+        me.rotation = vector.Math.multiplyMatrices(me.rollM, vector.Math.multiplyMatrices(me.pitchM, me.yawM));
+        me.rotation = vector.Math.multiplyMatrices(vm, me.rotation);
+
+        me.coord_x = math.cos(me.brng*D2R)*math.cos(me.ptch*D2R);
+        me.coord_y = -math.sin(me.brng*D2R)*math.cos(me.ptch*D2R);
+        me.coord_z = math.sin(me.ptch*D2R);
+        
+        var tv = [me.coord_x,me.coord_y,me.coord_z];
+        var dv = vector.Math.multiplyMatrixWithVector(me.rotation, tv);
+        var angles = vector.Math.cartesianToEuler(dv);
+
+        return [angles[0]==nil?0:angles[0],angles[1]];
+    },
+
+    getDevFromHMD: func (heading, pitch, viewh, viewp) {
+        # return pos in canvas from center origin
+        
+        var ym = vector.Math.yawMatrix(viewh);
+        var pm = vector.Math.pitchMatrix(-viewp);
+        var vm = vector.Math.multiplyMatrices(pm, ym);
+
+        me.target_x = math.cos(heading*D2R)*math.cos(pitch*D2R);
+        me.target_y = -math.sin(heading*D2R)*math.cos(pitch*D2R);
+        me.target_z = math.sin(pitch*D2R);
+        
+        var tv = [me.target_x,me.target_y,me.target_z];
+        var dv = vector.Math.multiplyMatrixWithVector(vm, tv);
+        var angles = vector.Math.cartesianToEuler(dv);
+
+        return [angles[0]==nil?0:angles[0],angles[1]];
+    },
 	
 	getPosFromDegs:  func (yaw_deg, pitch_deg) {
 		# return pos from bore
@@ -216,6 +263,7 @@ var HudMath = {
 	},
 	
 	getPolarFromCenterPos: func (x,y) {
+		# only works well when not too far from bore.
 		y -= me.centerOffset;
 		return me.getPolarFromBorePos(x,y);
 	},
@@ -298,7 +346,7 @@ var HudMath = {
 		}
 	},
 	
-	getDynamicHorizon: func (averagePoint_deg = 7.5, xMin=1,xMax=1,yMin=1,yMax=1) {
+	getDynamicHorizon: func (averagePoint_deg = 7.5, xMin=1,xMax=1,yMin=1,yMax=1,drift=1, drift_fix=0.0) {
 		# get translation and rotation for horizon line, dynamic means centered around FPI.
 		# the min max values are faction from center to edge of hud to restrict ladder movement.
 		# should be called after getFlightPathIndicatorPos/getFlightPathIndicatorPosWind.
@@ -306,8 +354,8 @@ var HudMath = {
 		
 		me.rot = -me.input.roll.getValue() * D2R;
 
-		me.pos_x_clamp = me.clamp(me.pos_x, -xMin*me.canvasWidth*0.5,xMax*me.canvasWidth*0.5);
-		me.pos_y_clamp = me.clamp(me.pos_y, -yMin*me.canvasHeight*0.5,yMax*me.canvasHeight*0.5);
+		me.pos_x_clamp = drift?me.clamp(me.pos_x, -xMin*me.canvasWidth*0.5,xMax*me.canvasWidth*0.5):0;
+		me.pos_y_clamp = drift?me.clamp(me.pos_y, -yMin*me.canvasHeight*0.5,yMax*me.canvasHeight*0.5):drift_fix*me.canvasHeight;
 
 	    # now figure out how much we move horizon group laterally, to keep FPI in middle of it.
 	    me.pos_y_rel = me.pos_y_clamp - me.getCenterOffset();

commit faa1c3cc5fada71ea9bb30d6d15c3b55757368fe

RWR: Fix nasal error (incorrect member name)
Error introduced in commit 773f3dc (RWR Update (#67), 2022-01-23)

Nasal/RADAR/rwr.nas
@@ -431,7 +431,7 @@ RWRCanvas = {
                 break;
             }
             if (me.typ == nil) {
-                if (me.contact[0].propNode.getName() != "multiplayer") {
+                if (me.contact[0].prop.getName() != "multiplayer") {
                     # AI planes are allowed to be unknowns
                     me.typ = me.AIRCRAFT_UNKNOWN;
                     me.unk = 1;